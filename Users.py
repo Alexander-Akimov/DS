f6f303ca35a7fc37537bca4378323b906a7ae78b
Alexander <a.akimov@dostek.kg>
2019-11-11 12:21:29 +0600

    paid accounts, topics of interest

--- Users.py
+++ Users.py
@@ -4,66 +4,71 @@ Created on Thu Oct 31 14:14:00 2019
 
 @author: Lex
 """
+from collections import defaultdict
+from collections import Counter
 users = [
-    { "id": 0, "name": "Hero" },
-    { "id": 1, "name": "Dunn" },
-    { "id": 2, "name": "Sue" },
-    { "id": 3, "name": "Chi" },
-    { "id": 4, "name": "Thor" },
-    { "id": 5, "name": "Clive" },
-    { "id": 6, "name": "Hicks" },
-    { "id": 7, "name": "Devin" },
-    { "id": 8, "name": "Kate" },
-    { "id": 9, "name": "Klein" }
+    {"id": 0, "name": "Hero"},
+    {"id": 1, "name": "Dunn"},
+    {"id": 2, "name": "Sue"},
+    {"id": 3, "name": "Chi"},
+    {"id": 4, "name": "Thor"},
+    {"id": 5, "name": "Clive"},
+    {"id": 6, "name": "Hicks"},
+    {"id": 7, "name": "Devin"},
+    {"id": 8, "name": "Kate"},
+    {"id": 9, "name": "Klein"}
 ]
 
-#print(users)
+# print(users)
 
 friendship_pairs = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
                     (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
 
 
-#print(friendship_pairs)
+# print(friendship_pairs)
 
 # Initialize the dict with an empty list for each user id:
 friendships = {user["id"]: [] for user in users}
 
-#print(friendships)
+# print(friendships)
 
 # And loop over the friendship pairs to populate it:
 for i, j in friendship_pairs:
     friendships[i].append(j)  # Add j as a friend of user i
     friendships[j].append(i)  # Add i as a friend of user j
-    
-#print(friendships)
-#____________________________________________________________________________
+
+# print(friendships)
+# ____________________________________________________________________________
+
+
 def number_of_friends(user):
     """How many friends does _user_ have?"""
     user_id = user["id"]
     friend_ids = friendships[user_id]
     return len(friend_ids)
 
-total_connections = sum(number_of_friends(user)
-    for user in users)
 
-num_users = len(users) # length of the users list
+total_connections = sum(number_of_friends(user)
+                        for user in users)
+
+num_users = len(users)  # length of the users list
 
 avg_connections = total_connections / num_users
-#print(avg_connections)
-#____________________________________________________________________________
+# print(avg_connections)
+# ____________________________________________________________________________
 
-#Create a list(user_id, number_of_freinds)
+# Create a list(user_id, number_of_freinds)
 num_friends_by_id = [(user["id"], number_of_friends(user))
-    for user in users]
+                     for user in users]
 
-#print(num_friends_by_id)
+# print(num_friends_by_id)
 
 num_friends_by_id.sort(
-        key=lambda id_and_friends: id_and_friends[1],
-        reverse=True)
-#print(num_friends_by_id)
-#____________________________________________________________________________
-from collections import Counter
+    key=lambda id_and_friends: id_and_friends[1],
+    reverse=True)
+# print(num_friends_by_id)
+# ____________________________________________________________________________
+
 
 def friends_of_friends(user):
     user_id = user["id"]
@@ -74,13 +79,14 @@ def friends_of_friends(user):
         if foaf_id != user_id
         and foaf_id not in friendships[user_id]
     )
-    
-#print(friends_of_friends(users[0])) #{3: 2}
 
-#print(friends_of_friends(users[3])) #mutual friends
+# print(friends_of_friends(users[0])) #{3: 2}
 
-#print(Counter([3,3,5,5,5])) #{5: 3, 3: 2}
-#____________________________________________________________________________
+# print(friends_of_friends(users[3])) #mutual friends
+
+
+# print(Counter([3,3,5,5,5])) #{5: 3, 3: 2}
+# ____________________________________________________________________________
 interests = [
     (0, "Hadoop"), (0, "Big Data"), (0, "HBase"), (0, "Java"),
     (0, "Spark"), (0, "Storm"), (0, "Cassandra"),
@@ -98,61 +104,64 @@ interests = [
     (9, "Java"), (9, "MapReduce"), (9, "Big Data")
 ]
 
+
 def data_scientiests_who_like(target_interest):
     """Find the ids of all users who like target interest."""
     return [user_id
             for user_id, user_interest in interests
             if user_interest == target_interest]
 
-from collections import defaultdict
 
 user_ids_by_interest = defaultdict(list)
 
 for user_id, interest in interests:
     user_ids_by_interest[interest].append(user_id)
-#_________________________    
+# _________________________
 
 interests_by_user_id = defaultdict(list)
 
 for user_id, interest in interests:
     interests_by_user_id[user_id].append(interest)
-#_________________________    
-    
-#print(user_ids_by_interest)
-#print(interests_by_user_id)
+# _________________________
+
+# print(user_ids_by_interest)
+# print(interests_by_user_id)
+
 
 def most_common_interests_with(user):
     return Counter(
-            interested_user_id
-            for interest in interests_by_user_id[user["id"]]
-            for interested_user_id in user_ids_by_interest[interest]
-            if interested_user_id != user["id"]
+        interested_user_id
+        for interest in interests_by_user_id[user["id"]]
+        for interested_user_id in user_ids_by_interest[interest]
+        if interested_user_id != user["id"]
     )
-    
-#print(users[3])
-#print(most_common_interests_with(users[3]))
-#____________________________________________________________________________
+
+# print(users[3])
+# print(most_common_interests_with(users[3]))
+# ____________________________________________________________________________
+
 
 salaries_and_tenures = [(83000, 8.7), (88000, 8.1),
                         (48000, 0.7), (76000, 6),
                         (69000, 6.5), (76000, 7.5),
                         (60000, 2.5), (83000, 10),
                         (48000, 1.9), (63000, 4.2)]
-#________________________________________________
+# ________________________________________________
 
 salary_by_tenure = defaultdict(list)
 
 for salary, tenure in salaries_and_tenures:
     salary_by_tenure[tenure].append(salary)
-#print(salary_by_tenure)
+# print(salary_by_tenure)
 
 average_salary_by_tenure = {
-        tenure: sum(salaries) / len(salaries)
-        for tenure, salaries in salary_by_tenure.items()
+    tenure: sum(salaries) / len(salaries)
+    for tenure, salaries in salary_by_tenure.items()
 }
-    
-#print(average_salary_by_tenure)    
-    
+
+# print(average_salary_by_tenure)
+
+
 def tenure_bucket(tenure):
     if tenure < 2:
         return "less than two"
@@ -160,25 +169,51 @@ def tenure_bucket(tenure):
         return "between two and five"
     else:
         return "more than five"
-#________________________________________________
+
+
+# ________________________________________________
 salary_by_tenure_bucket = defaultdict(list)
 
 for salary, tenure in salaries_and_tenures:
     bucket = tenure_bucket(tenure)
     salary_by_tenure_bucket[bucket].append(salary)
 
-print(salary_by_tenure_bucket)    
-    
-    
+# print(salary_by_tenure_bucket)
+
+
 average_salary_by_bucket = {
-        tenure_bucket: sum(salaries) / len(salaries)
-        for tenure_bucket, salaries in salary_by_tenure_bucket.items()
+    tenure_bucket: sum(salaries) / len(salaries)
+    for tenure_bucket, salaries in salary_by_tenure_bucket.items()
 }
-print(average_salary_by_bucket)
-    
-    
-    
-    
-    
-    
-    
+# print(average_salary_by_bucket)
+# _________________________________________________
+
+years_of_experience = [0.7, 1.9, 2.5, 4.2, 6.0, 6.5, 7.5, 8.1, 8.7, 10.0]
+
+
+def predict_paid_or_unpaid(years_experience):
+    if years_experience < 3.0:
+        return "paid"
+    elif years_experience < 8.5:
+        return "unpaid"
+    else:
+        return "paid"
+
+
+tenure_account = {
+    tenure: predict_paid_or_unpaid(tenure)
+    for tenure in years_of_experience
+}
+
+# print(tenure_account)
+# _________________________________________________
+
+words_and_counts = Counter(word
+                           for user, interest in interests
+                           for word in interest.lower().split())
+
+for word, count in words_and_counts.most_common():
+    if count > 1:
+        print(word, count)
+
+# _________________________________________________
